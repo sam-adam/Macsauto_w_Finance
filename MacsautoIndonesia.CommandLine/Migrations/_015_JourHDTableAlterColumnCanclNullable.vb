d45304c87bf7c71200e515e8ff205d5dc44e7dfa
sam-adam <samuel.suhendra@gmail.com>
2015-01-06 21:36:56 +0700

    Migration jourdt cancl nullable

---
+++ MacsautoIndonesia.CommandLine/Migrations/_015_JourHDTableAlterColumnCanclNullable.vb
@@ -0,0 +1,18 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(201501062134)>
+    Public Class _015_JourHDTableAlterColumnCanclNullable
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "jourhd"
+
+        Public Overrides Sub Up()
+            Alter.Column("cancl").OnTable(_tableName).AsString(1).Nullable()
+        End Sub
+
+        Public Overrides Sub Down()
+            Alter.Column("cancl").OnTable(_tableName).AsString(1).NotNullable()
+        End Sub
+    End Class
+End Namespace
