a15c1415d75cba6a5642a54d35be9ed85085b0cb
sam-adam <samuel.suhendra@gmail.com>
2015-01-06 20:48:22 +0700

    Added migration hproduct and producttype

---
+++ MacsautoIndonesia.CommandLine/Migrations/_011_HProductTableAlterAddColumnGLNum.vb
@@ -0,0 +1,18 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(201501062025)>
+    Public Class _011_HProductTableAlterAddColumnGLNum
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "hproduct"
+
+        Public Overrides Sub Up()
+            Alter.Table(_tableName).AddColumn("glnum").AsString(10).Indexed()
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("glnum").FromTable(_tableName)
+        End Sub
+    End Class
+End Namespace
---
+++ MacsautoIndonesia.CommandLine/Migrations/_012_ProductTypeTableAlterAddColumnIsCnsm.vb
@@ -0,0 +1,18 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(201501062031)>
+    Public Class _012_ProductTypeTableAlterAddColumnIsCnsm
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "producttype"
+
+        Public Overrides Sub Up()
+            Alter.Table(_tableName).AddColumn("glnum").AsBoolean().Indexed()
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("glnum").FromTable(_tableName)
+        End Sub
+    End Class
+End Namespace
