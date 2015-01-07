adee2a474e7320398461a4249bbdf06f0e8df5cf
sam-adam <samuel.suhendra@gmail.com>
2015-01-07 20:22:04 +0700

    #3 - Migration hproduct and hservice is_active column

---
+++ MacsautoIndonesia.CommandLine/Migrations/_016_HProductTableAlterAddIsActive.vb
@@ -0,0 +1,18 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(201501071958)>
+    Public Class _016_HProductTableAlterAddIsActive
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "hproduct"
+
+        Public Overrides Sub Up()
+            Alter.Table(_tableName).AddColumn("is_active").AsBoolean().NotNullable().WithDefaultValue(1)
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("is_active").FromTable(_tableName)
+        End Sub
+    End Class
+End Namespace
---
+++ MacsautoIndonesia.CommandLine/Migrations/_017_HServiceTableAlterAddIsActive.vb
@@ -0,0 +1,18 @@
+﻿Imports FluentMigrator
+
+Namespace Migrations
+    <Migration(201501072020)>
+    Public Class _017_HServiceTableAlterAddIsActive
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "hservice"
+
+        Public Overrides Sub Up()
+            Alter.Table(_tableName).AddColumn("is_active").AsBoolean().NotNullable().WithDefaultValue(1)
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("is_active").FromTable(_tableName)
+        End Sub
+    End Class
+End Namespace
