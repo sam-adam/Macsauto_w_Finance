c1367d294c4470ec97a7b8dc80baa18409ab24a3
surwijaya12 <surya.wijaya12@gmail.com>
2015-01-08 16:51:50 +0700

    Update Migration for Petty Cash
    Add Pettycash-CRTDT (Created Date)

--- MacsautoIndonesia.CommandLine/Migrations/_018_PettyCashTableAlterColumnCRTDTNullable.vb
+++ MacsautoIndonesia.CommandLine/Migrations/_018_PettyCashTableAlterColumnCRTDTNullable.vb
@@ -1,3 +1,18 @@
-﻿Public Class _018_PettyCashTableAlterColumnCRTDTNullable
+﻿Imports FluentMigrator
 
-End Class
+Namespace Migrations
+    <Migration(201501091532)>
+    Public Class _018_PettyCashTableAlterColumnCRTDTNullable
+        Inherits Migration
+
+        Private ReadOnly _tableName As String = "pettycash"
+
+        Public Overrides Sub Up()
+            Alter.Table(_tableName).AddColumn("crtdt").AsDate.NotNullable().WithDefaultValue(False)
+        End Sub
+
+        Public Overrides Sub Down()
+            Delete.Column("crtdt").FromTable(_tableName)
+        End Sub
+    End Class
+End Namespace
